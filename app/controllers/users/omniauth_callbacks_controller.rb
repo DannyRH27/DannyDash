f12abcba81941a7edd5f72694aa19086d66a102f
Danny <danny.r.huang@gmail.com>
2020-06-20 11:13:46 -0700

    add cart to omni-auth user

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         if @user.persisted?
             login!(@user)
             redirect_to "#/home"
-
+            @cart = @user.cart || Cart.create(customer_id: @user.id)
             sign_in @user, event: :authentication
             set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
         else
