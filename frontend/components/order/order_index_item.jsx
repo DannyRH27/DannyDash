8b8bddd5032b647c5d5fe91dc6e00ecdf255f41e
Danny <danny.r.huang@gmail.com>
2020-06-19 21:15:21 -0700

    create previous order index

--- frontend/components/order/order_index_item.jsx
+++ frontend/components/order/order_index_item.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { IoIosCheckmarkCircle } from "react-icons/io";
 
 function OrderIndexItem(props) {
   const { order } = props;
-  return (
-    <div className="order-index-item-container">
+  const date = new Date();
+  const month = date.getMonth();
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const strDate = `${months[month]} ${day} ${year}, ${order.deliveryEta}  •  $${order.total}`;
+  const orderContents =  Object.values(order.contents)
+  const items = orderContents.map((item) => (
+    <span key={item.id}>{item.name}, &nbsp;</span>
+  ));
+  items[items.length - 1] = <span key={orderContents[orderContents.length-1].id}>{orderContents[orderContents.length-1].name.substr(0, orderContents[orderContents.length-1].name.length)}</span>  
 
+
+  console.log(orderContents)
+  return (
+    <div className="order-index-item">
+      <div className="order-item-title">
+        <span>{order.store.name}</span>
+        <IoIosCheckmarkCircle />
+      </div>
+      <span>{strDate}</span>
+      <div id="order-item-summary">
+        {items}
+      </div>
     </div>
   );
 }
