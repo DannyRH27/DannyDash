3ab6918c73928e59b061b7ec26b178bd290f4322
Danny <danny.r.huang@gmail.com>
2020-07-25 16:09:30 -0700

    code cleanup

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { RiNetflixLine } from "react-icons/ri";
 import StoreIndexItem from "../store_index/store_index_item";
 class Splash extends React.Component{
   constructor(props){
