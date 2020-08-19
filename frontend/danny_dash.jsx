a2566d90d4f207dee72ec32afcbf38f015b2a66c
Danny <danny.r.huang@gmail.com>
2020-08-18 17:43:24 -0700

    code cleanup

--- frontend/danny_dash.jsx
+++ frontend/danny_dash.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./components/root";
 import configureStore from "./store/store";
-import {login, logout, signup} from "./actions/session_actions";
-import { openModal } from "./actions/modal_actions";
-import { fetchCart, updateCart } from "./actions/cart_actions";
-import { searchStores } from "./util/store_api_util";
-import { filterStores } from "./actions/store_actions";
+
 document.addEventListener("DOMContentLoaded", () => {
 	let preloadedState = undefined;
 	if (window.currentUser) {
